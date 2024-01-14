33bf75720902f62d581f8e275717fa7c0398f08c
NaveenHaswani <153353041+NaveenHaswani@users.noreply.github.com>
2024-01-14 15:24:12 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -1,2 +1,2 @@
-def call(String repositoryUrl = 'https://github.com/NaveenHaswani/Jenkinstest.git', Branch = 'Main') {
+def call(String repositoryUrl = 'https://github.com/NaveenHaswani/Jenkinstest.git', branch = 'Main') {
     git branch: branch, url: repositoryUrl
