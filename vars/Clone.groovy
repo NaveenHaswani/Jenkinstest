75bc229e404c556a8e7311398961e32f6a247c6c
NaveenHaswani <153353041+NaveenHaswani@users.noreply.github.com>
2024-01-14 15:37:58 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -1,2 +1,2 @@
-def call(String repositoryUrl = 'https://github.com/NaveenHaswani/Jenkinstest.git', branch = 'Main') {
+def call(String repositoryUrl = 'https://github.com/opstree/spring3hibernate.git', branch = 'java7') {
     git branch: branch, url: repositoryUrl }
